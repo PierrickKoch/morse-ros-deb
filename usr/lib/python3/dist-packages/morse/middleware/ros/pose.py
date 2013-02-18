7272ef76d827078e8d510438b5de50242eee55f7
Pierrick Koch <pierrick.koch@gmail.com>
2013-02-18 11:44:38 +0100

    init from morse 8d4f395

---
+++ usr/lib/python3/dist-packages/morse/middleware/ros/pose.py
@@ -0,0 +1,24 @@
+import logging; logger = logging.getLogger("morse." + __name__)
+import roslib; roslib.load_manifest('geometry_msgs')
+from geometry_msgs.msg import PoseStamped
+from morse.middleware.ros import ROSPublisher, mathutils
+
+class PoseStampedPublisher(ROSPublisher):
+    """ Publish the position and orientation of the robot. """
+    ros_class = PoseStamped
+    default_frame_id = '/map'
+
+    def default(self, ci='unused'):
+        pose = PoseStamped()
+        pose.header = self.get_ros_header()
+
+        pose.pose.position.x = self.data['x']
+        pose.pose.position.y = self.data['y']
+        pose.pose.position.z = self.data['z']
+
+        euler = mathutils.Euler((self.data['roll'],
+                                 self.data['pitch'],
+                                 self.data['yaw']))
+        pose.pose.orientation = euler.to_quaternion()
+
+        self.publish(pose)
